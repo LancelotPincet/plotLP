45b61796b62a0ad2330ce5245d24b3c34dd262e7
LancelotPincet <lancelot.pincet@free.fr>
2025-12-12 07:58:45 +0100

    auto-commit

--- src/plotlp/modules/subplots_LP/test_subplots.py
+++ src/plotlp/modules/subplots_LP/test_subplots.py
@@ -34,15 +34,16 @@ def test_subplot() :
     R = np.sqrt(X**2+Y**2)
     img = np.exp(-R**2/2/4**2)
 
-    fig = figure(figsize_fact=(0.5,0.5), darkmode=True)
-    fig.axis.plot(x, -y)
-    plt.plot(x, y)
-    plt.xlabel('x [ua]')
-    fig.axis.set_ylabel('y [ua]')
+    fig, ax = subplots(2, 2, darkmode=True)
+    ax[0,0].plot(x, -y)
+    ax[0,1].plot(x, y)
+    ax[1,0].imshow(img, barname='intensity')
+    fig.axis = 0
+    plt.xlabel('axis 0')
+    fig.axis = 1
+    fig.axis.set_ylabel('axis 1')
     fig.title = 'test title'
-    plt.savefig(path_png = debug_folder / 'plot_png', close=False)
-    plt.savefig(path_pdf = debug_folder / 'plot_pdf', close=False)
-    plt.savefig(debug_folder / 'plot_all')
+    plt.savefig(path_png = debug_folder / 'subplot_png', close=False)
 
 
 
