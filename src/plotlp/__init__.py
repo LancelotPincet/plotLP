5d861bde5044490bd9d2c1fe64aef76d8c2562f7
LancelotPincet <lancelot.pincet@free.fr>
2026-01-21 19:48:08 +0100

    auto-commit

--- src/plotlp/__init__.py
+++ src/plotlp/__init__.py
@@ -11,9 +11,24 @@ A library wrapper around matplotlib for custom plots.
 
 
 
+# %% Source code
+sources = {
+'StyledAxes': 'plotlp.modules.StyledAxes_LP.StyledAxes',
+'StyledFigure': 'plotlp.modules.StyledFigure_LP.StyledFigure',
+'cmap': 'plotlp.modules.cmap_LP.cmap',
+'color': 'plotlp.modules.color_LP.color',
+'figure': 'plotlp.modules.figure_LP.figure',
+'imgfigure': 'plotlp.modules.imgfigure_LP.imgfigure',
+'plt': 'plotlp.modules.plt_LP.plt',
+'style': 'plotlp.modules.style_LP.style',
+'subplots': 'plotlp.modules.subplots_LP.subplots'
+}
+
+
+
 # %% Lazy imports
 from corelp import getmodule
-__getattr__, __all__ = getmodule(__file__)
+__getattr__, __all__ = getmodule(sources)
 
 
 
